fc5c4bcc69dc68080dbc9ebeeb32ff5510ac4a2e
Ying Xi <22023689@myrp.edu.sg>
2023-08-16 14:47:43 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id22023689/mywebbrowser/MainActivity.java
@@ -0,0 +1,35 @@
+package sg.edu.rp.c346.id22023689.mywebbrowser;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+    Button btnLoadURL;
+    WebView wvMyPage;
+
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLoadURL = findViewById(R.id.btnLoadURL);
+        wvMyPage = findViewById(R.id.wvMyPage);
+
+        wvMyPage.setWebViewClient(new WebViewClient());
+
+        btnLoadURL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = "https://www.rp.edu.sg/soi/home";
+                wvMyPage.loadUrl(url);
+            }
+        });
+    }
+}
